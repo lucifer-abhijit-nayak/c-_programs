f01d7eebfd24577d809dcdb9fd7e8e8b88f56781
lucifer-abhijit-nayak <64629667+lucifer-abhijit-nayak@users.noreply.github.com>
2020-05-01 11:53:06 +0530

    size.c
    will print output regarding size of char, int, float and double respectively

---
+++ size.c
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+ using namespace std;
+int main()
+{
+  cout<<"size of char:"<<sizeof(char)<<" byte"<<'\n';
+  cout<<"size of int:"<<sizeof(int)<<" byte"<<'\n';
+  cout<<"size of float:"<<sizeof(float)<<" byte"<<'\n';
+  cout<<"size of double:"<<sizeof(double)<<" byte"<<'\n';
