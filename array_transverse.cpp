1d472aa06024d88339d82a671ea9bd03357ba177
Abhijit nayak <64629667+lucifer-abhijit-nayak@users.noreply.github.com>
2020-05-25 20:56:03 +0530

    Create array_transverse.cpp

---
+++ array_transverse.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+ using naemspace std;
+int main()
+{
+ int A[5];
+ cout<<"Enter elements:";
+ for(int i=0;i<5;i++)
+ {
+   cin>>A[i];
+ }
+ cout<<"You entered:";
+ for(int i=0;i<5;i++)
+ {
+    cout<<A[i]<<'\n';
+ }
